50c2f316222cc5b9fd7fe1cc29ca2b3d612139b5
ycq <1447589683@qq.com>
2022-05-07 13:21:47 +0800

    Hello World

---
+++ react-learn/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));;
+
+root.render(<h1 title="第一个React元素">hello, world!<span>你好啊!</span></h1>);
