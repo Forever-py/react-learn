879edfdcd7a616511c171d5a07d88ea49bb94946
ycq <1447589683@qq.com>
2022-05-09 14:18:08 +0800

    组件状态

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// https://open.duyiedu.com/api/student/findAll?appkey=jerry_1602593073067
-
-import StudentsList from './components/StudentsList';
+import Tick from './componenets/Tick';
 const root = ReactDOM.createRoot(document.getElementById('root'))
+let number = 10;
+// function start() {
+//     const timer = setInterval(() => {
+//         number--;
+//         if (number === 0) {
+//             clearInterval(this.timer);
+//             return;
+//         }
+//         root.render(<Tick number={number} />)
+//     }, 1000)
+// }
 
-const appkey = "jerry_1602593073067";
+// start();
 
-/**
- * 获取所有的学生数据
- */
-async function fetchAllStudents() {
-    
-    // resp.json() 解析成json格式
-    const stus = await fetch("https://open.duyiedu.com/api/student/findAll?appkey=" + appkey).then(resp => resp.json()).then(resp => resp.data);
-    return stus
-}
-
-async function render() {
-    root.render(<div>正在加载中...</div>)
-    const stus = await fetchAllStudents(); // 获取学生数据
-    root.render(<ul>
-        <StudentsList stus={stus} />
-    </ul>)
-}
-
-render();
+root.render(<Tick number={number} />)
