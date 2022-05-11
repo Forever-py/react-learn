9d158c077da4602f861c801205bd63eddb8bcd57
ycq <1447589683@qq.com>
2022-05-11 16:03:37 +0800

    深入认识setState

---
+++ react-learn/src/componenets/Comp.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class Comp extends Component {
+
+    state = {
+        n: 0,
+    }
+    handleClick = () => {
+        // this.setState({ n: this.state.n + 1 }, ()=> {
+        //     // 状态完成改变之后触发   该回调运行在render之后
+        //     console.log(this.state.n);
+        // });
+
+        this.setState(cur => {
+            // 参数 cur 表示当前的状态
+            // 该函数的返回结果，会混合(覆盖)掉之前的状态
+            // 该函数是异步执行
+            return {
+                n: cur.n + 1
+            }
+        })
+        this.setState(cur=> ({n: cur.n + 1}));
+        this.setState(cur=> ({n: cur.n + 1}));
+
+        // console.log(this.state.n); // 还没有重新渲染,说明目前状态仍然没有改变
+    }
+    render() {
+        console.log('render');
+        return (
+            <div>
+                <h1>{this.state.n}</h1>
+                <button onClick={this.handleClick}>+</button>
+            </div>
+        )
+    }
+}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Pager from './componenets/Pager';
-import PagerTest from './componenets/PagerTest';
-
+import Comp from './componenets/Comp';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// let obj = {
-//     current: 3,
-//     total: 100,
-//     limit: 9,
-//     panelNumber: 5
-// }
 
-
-root.render(<PagerTest/>)
+root.render(<div><Comp /></div>)
