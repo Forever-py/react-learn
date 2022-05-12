3091717e4109605332a16c9536fcd6313fb4028f
ycq <1447589683@qq.com>
2022-05-12 15:21:07 +0800

    React旧生命周期

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Comp from './componenets/Comp';
+import App from './componenets/App';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+// // 函数组件  可以理解为没有生命周期
+// function A () {
+//     return <h1>aaaaaa</h1>
+// }
 
-root.render(<div><Comp /></div>)
+// setInterval(()=> {
+//     root.render(<A/>)
+// }, 1000)
+
+// class A {
+//     render() {
+//         return null
+//     }
+// }
+
+// root.render(<>
+//     <A /> {/* 相当于 new A */}
+//     <A /> {/* 组件实例 */}
+//     <A />
+// </>)
+
+
+root.render(<div><App/></div>)
